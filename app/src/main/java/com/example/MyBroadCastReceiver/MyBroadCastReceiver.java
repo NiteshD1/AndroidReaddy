ef4583533fe0aeecb6e8fd10eaf8976b38b4501a
Nitesh Dewada <nitesh.dewada@go-upwards.com>
2022-10-23 18:51:44 +0530

    Changes done

--- app/src/main/java/com/example/MyBroadCastReceiver/MyBroadCastReceiver.java
+++ app/src/main/java/com/example/MyBroadCastReceiver/MyBroadCastReceiver.java
@@ -3,13 +3,16 @@ package com.example.MyBroadCastReceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
 import com.example.android_ready.utils.Utils;
 
 public class MyBroadCastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Utils.print("called");
-        Utils.showToast("MyBroadCastReceiver onReceive called ");
+        if (intent.getAction() == Intent.ACTION_AIRPLANE_MODE_CHANGED){
+            Utils.showToast("Airplane Mode Changed");
+            Utils.print(intent);
+        }
     }
 }
--- app/src/main/java/com/example/android_ready/MainActivity.java
+++ app/src/main/java/com/example/android_ready/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android_ready;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,21 +13,36 @@ import android.view.View;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.MyBroadCastReceiver.MyBroadCastReceiver;
 import com.example.android_ready.databinding.ActivityMainBinding;
 import com.example.android_ready.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-
+    MyBroadCastReceiver myReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-
-
     }
 
+    @Override
+    protected void onStart() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.intent.action.AIRPLANE_MODE");
+
+        myReceiver = new MyBroadCastReceiver();
+        registerReceiver(myReceiver, intentFilter);
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+
+        unregisterReceiver(myReceiver);
+        super.onStop();
+    }
 }
